bd62e16f473e19164a4a256c6eab84501c7d6b69
Art Goldhammer <art.goldhammer@gmail.com>
2021-03-21 11:30:02 -0400

    dead code removed

--- proc.go
+++ proc.go
@@ -47,37 +47,6 @@ func readAuths(client *mongo.Client) {
 	}
 }
 
-func readStatuses(client *mongo.Client, searchtext *string) {
-	var status StatusType
-	//pnc := make(properNounCounterType, 1000)
-
-	ctx := context.TODO()
-
-	clause1 := primitive.E{Key: "$search", Value: searchtext}
-	//clause2 := primitive.E{Key: "$text", Value: clause1}
-	// fmt.Printf("clause2 %v", clause2)
-
-	searchfor := bson.D{primitive.E{Key: "$text", Value: bson.D{clause1}}}
-	//searchfor := bson.D{}
-	findOptions := options.Find()
-	findOptions.SetLimit(50000)
-	statuses := client.Database("euronews").Collection("statuses")
-
-	cur, err := statuses.Find(ctx, searchfor, findOptions)
-	defer cur.Close(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for cur.Next(context.TODO()) {
-		if err := cur.Decode(&status); err != nil {
-			log.Fatal(err)
-		}
-		//fmt.Printf("status: %v\n", cur.Current)
-		fmt.Println("--->", status.Text)
-		fmt.Println("***: ", status.Created_at.Time())
-	}
-}
-
 func estCount(client *mongo.Client) {
 	statuses := client.Database("euronews").Collection("statuses")
 	opts := options.EstimatedDocumentCount().SetMaxTime(2 * time.Second)
